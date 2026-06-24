3963b9a32faa827e5c9287550a3d1b85f0d44b0f
hour <Songhour10dl@gmail.com>
2026-06-24 21:26:28 +0700

    refreshing react

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,3 +1,14 @@
-function App() {}
+import Header from "./Header";
+import Footer from "./Footer";
+import Food from "./Food";
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+      <Food></Food>
+    </>
+  );
+}
 
 export default App;
