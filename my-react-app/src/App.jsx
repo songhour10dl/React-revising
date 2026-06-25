a7cb6e62acf3f6cdb0a1683daa6d23b6298936f6
hour <Songhour10dl@gmail.com>
2026-06-25 10:03:59 +0700

    initialize the 3 way to style components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,8 @@
-import Card from "./Card";
+import Button from "./Button/Button";
 function App() {
   return (
     <>
-      <Card></Card>
+      <Button></Button>
     </>
   );
 }
---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,9 @@
+function Button() {
+  return (
+    <>
+      <button className={styles.button}>Click me </button>
+    </>
+  );
+}
+
+export default Button;
